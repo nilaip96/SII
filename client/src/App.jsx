bd3ad9c9331a662ac471ba217edb692443ca9eb3
Nilai Patel <nilaip96@gmail.com>
2021-05-19 21:09:57 -0700

    Added CSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,68 @@
 import React from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
+import styles from './styles.jsx';
+
+const Wrapper =  styled.div`
+  width: 101vw;
+  height: 101vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: ${styles.primary};
+`;
+
+const Title = styled.div`
+  margin: 10vh 0;
+  font-size: 2vw;
+  color: ${styles.light};
+`;
+
+const Input = styled.input`
+  width: 50vw;
+  height: 3vh;
+  background: ${styles.light};
+  color: ${styles.primary};
+`;
+
+const Submit = styled.button`
+  width: 10vw;
+  height: 3vh;
+  margin: 1vh 1vh;
+  background: ${styles.light};
+  color: ${styles.primary};
+`;
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      input: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(e){
+    this.setState({[e.target.name]:e.target.value})
+  }
+
+  handleClick(e){
+    axios.get(`/temp/${this.state.input}`)
+      .then(({data}) =>{console.log(data)})
+    this.setState({input:''})
+  }
+  
+
   render() {
     return (
-      <div>Hello World</div>
+      <Wrapper>
+        <Title>
+          Hello World
+        </Title>
+        <Input value={this.state.input} onChange={this.handleChange} name="input"/>
+        <Submit onClick={this.handleClick}>Submit</Submit>
+      </Wrapper>
     )
   }
 }
---
+++ client/src/styles.jsx
@@ -0,0 +1,7 @@
+const styles = {
+  primary: '#4169E1',
+  secondary: '#F5F5F5',
+  light: '#FFFAFA'
+}
+
+export default styles;
